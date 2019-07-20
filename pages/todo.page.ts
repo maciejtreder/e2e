8c9321962c4d47987f055bd27bac336453ee7357
Maciej Treder <contact@maciejtreder.com>
2019-07-20 11:08:23 +0200

    aaa

--- pages/todo.page.ts
+++ pages/todo.page.ts
@@ -1,14 +1,29 @@
-import { WebElement, element, by, browser } from 'protractor';
+import { ElementFinder, element, by, browser } from 'protractor';
+import { BasePage } from './base.page';
 
-export class TodoPage {
-    private static title: WebElement = element(by.css('h1'));
-    private static newTaskInput: WebElement = element(by.tagName('input'));
-
-    public static async go() {
-        await browser.get(`http://maciejtreder.github.io/e2e/static/todos`);
+export class TodoPage extends BasePage {
+    public go() {
+        browser.get(`http://maciejtreder.github.io/e2e/static/todos`);
     }
 
-    public static async getTitle() {
-        return TodoPage.title.getText();
+    public async isOnPage(): Promise<boolean> {
+        return await this.getTitle() === 'Todo';
     }
+
+    public getNewTaskInput(): ElementFinder {
+        return element(by.tagName('input'));
+    }
+
+    public getNewTaskSubmitButton(): ElementFinder {
+        return element(by.xpath('//button[text()="Save"]'));
+    }
+
+    public getNewTaskError(): ElementFinder {
+        return element(by.css('span.error'));
+    }
+
+    public getTaskByName(name: string): ElementFinder {
+        return element(by.xpath(`//li[text()="${name}"]`));
+    }
+
 }
--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,8 +1,26 @@
-import { browser, by, element } from 'protractor';
+import { browser, by } from 'protractor';
+import { MenuComponent } from '../pages/menu.component';
+import { DonePage } from '../pages/done.page';
+import { TodoPage } from '../pages/todo.page';
 
 describe('Basic tests', () => {
-    it('Should be able to navigate to the application', async () => {
+
+    const menu: MenuComponent = new MenuComponent(by.tagName('menu'));
+    const donePage: DonePage = new DonePage();
+    const todoPage: TodoPage = new TodoPage();
+
+    beforeAll(() => {
         browser.get('https://maciejtreder.github.io/e2e/static');
-        expect(await element(by.tagName('menu')).isPresent()).toBeTruthy();
+    });
+
+    it('Should be able to navigate to the application', async () => {
+        expect(await menu.getContainer().isPresent()).toBeTruthy();
+    });
+
+    it('Should be able to navigate within the application', async () => {
+        menu.getItemByText('Done').click();
+        expect (await donePage.isOnPage()).toBeTruthy();
+        menu.getItemByText('Todo list').click();
+        expect (await todoPage.isOnPage()).toBeTruthy();
     });
 });
